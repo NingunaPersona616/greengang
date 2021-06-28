5ba71494cc036196c1d91c84fa04eadb0dbc3b48
Brandon Hernandez Ledezma <brandon.hernandez5150@alumnos.udg.mx>
2021-06-27 19:48:57 -0500

    Verificación de correo
    Envíos de correos  de verificación al crear cuentas.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProviderController;
 use App\Http\Controllers\PurchaseController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,11 +25,28 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+//Rutas para la verificacion por correos
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 //Rutas para los proveedores
 Route::resource('provider', ProviderController::class)->middleware('auth');
 
 //Rutas para los productos
-Route::resource('product', ProductController::class)->middleware('auth');
+Route::resource('product', ProductController::class)->middleware('verified');
 
 //Rutas para las compras
 Route::get('purchase/create', [PurchaseController::class, 'create'])->name('purchase.create')->middleware('auth');
